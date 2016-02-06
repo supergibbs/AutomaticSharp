5d358959ba8bd8c41ca4647acd0fe7851d84f65f
Jesse Mandel <jesse.mandel@gmail.com>
2016-02-06 13:31:36 -0800

    Fixing MyGet's bad PATH var....

--- pre-MyGet.ps1
+++ pre-MyGet.ps1
@@ -1,4 +1,7 @@
-$dnxRoot = [regex]::match($env:PATH,'([^;]+\\\.dnx\\runtimes\\[^;]+\\bin);').Groups[1].Value
+#This should work but it's not installed properly
+#$dnxRoot = [regex]::match($env:PATH,'([^;]+\\\.dnx\\runtimes\\[^;]+\\bin);').Groups[1].Value
+
+$dnxRoot = $dnxRoot = "C:\Users\Wonka\.dnx\runtimes\dnx-clr-win-x86.1.0.0-rc2-16357\bin"
 $dnx = "$dnxRoot\dnx.exe"
 $dnxTooling = "$dnxRoot\lib\Microsoft.Dnx.Tooling\Microsoft.Dnx.Tooling.dll"
  
