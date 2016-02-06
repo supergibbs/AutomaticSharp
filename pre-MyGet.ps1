94d533464319038f5e474bb5ee3017d504a19e9a
Jesse Mandel <jesse.mandel@gmail.com>
2016-02-06 13:24:03 -0800

    In PATH but not?

--- pre-MyGet.ps1
+++ pre-MyGet.ps1
@@ -1,4 +1,5 @@
 $dnxRoot = [regex]::match($env:PATH,'([^;]+\\\.dnx\\runtimes\\[^;]+\\bin);').Groups[1].Value
-$dnxTooling =  "$dnxRoot\lib\Microsoft.Dnx.Tooling\Microsoft.Dnx.Tooling.dll"
-
-& dnx $dnxTooling restore ".\src\AutomaticSharp"
+$dnx = "$dnxRoot\dnx.exe"
+$dnxTooling = "$dnxRoot\lib\Microsoft.Dnx.Tooling\Microsoft.Dnx.Tooling.dll"
+ 
+& $dnx $dnxTooling restore ".\src\AutomaticSharp"
