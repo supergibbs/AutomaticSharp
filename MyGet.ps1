11e91718a322dde0ee35ee24ab70eb48af1a4e49
Jesse Mandel <jesse.mandel@gmail.com>
2016-02-08 08:35:12 -0800

    Switched CI to AppVeyor

--- MyGet.ps1
+++
@@ -1,9 +0,0 @@
-#This should work but it's not installed properly
-#$dnxRoot = [regex]::match($env:PATH,'([^;]+\\\.dnx\\runtimes\\[^;]+\\bin);').Groups[1].Value
-
-$dnxRoot = $dnxRoot = "C:\Users\Wonka\.dnx\runtimes\dnx-clr-win-x86.1.0.0-rc2-16357\bin"
-$dnx = "$dnxRoot\dnx.exe"
-$dnxTooling = "$dnxRoot\lib\Microsoft.Dnx.Tooling\Microsoft.Dnx.Tooling.dll"
- 
-& $dnx $dnxTooling restore ".\src\AutomaticSharp"
-& $dnx --appbase ".\src\AutomaticSharp" $dnxTooling pack ".\src\AutomaticSharp" --configuration Release --out ".\artifacts\bin\AutomaticSharp"
---
+++ cmd/aspnet5-version.js
@@ -0,0 +1,22 @@
+var jsonfile = require('jsonfile');
+var semver = require('semver');
+
+var file = '../src/AutomaticSharp/project.json';
+var semversion;
+
+if (process.env.APPVEYOR_REPO_TAG) {
+    semversion = process.env.APPVEYOR_REPO_TAG_NAME.substring(1);
+} else {
+    var buildVersion = process.env.APPVEYOR_BUILD_VERSION.substring(1);
+    var findPoint = buildVersion.lastIndexOf(".");
+    var basePackageVer = buildVersion.substring(0, findPoint);
+    var buildNumber = buildVersion.substring(findPoint + 1, buildVersion.length);
+    semversion = semver.valid(basePackageVer + '-CI' + buildNumber);
+}
+
+jsonfile.readFile(file, function(err, project) {
+    project.version = semversion;
+    jsonfile.writeFile(file, project, { spaces: 2 }, function(err) {
+        console.error(err);
+    });
+});
